5060b93e23c49a24c4041edc23a0095fb0f99f75
Fatiha26 <f6lamia@gmail.com>
2024-10-08 23:48:06 +0600

    modified signup page

---
+++ src/app/component/button/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ children, type = "button", onClick }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="w-2/4 px-4 py-2 font-semibold text-white bg-[#3498DB] rounded-2xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mx-auto block"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
