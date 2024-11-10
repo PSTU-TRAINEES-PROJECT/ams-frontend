7d7de56423e626030737fde53f00428074390d06
sabuj_sarker <raj16@cse.pstu.ac.bd>
2024-11-11 01:24:48 +0600

    Here is my home page,footer,header and some changes of previous signup and signin page

--- my-app/src/app/(pages)/login/page.tsx
+++ my-app/src/app/(pages)/login/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signupSchema } from "../../schemas/signupSchema";
+import { loginSchema } from "../../schemas/loginSchema"; // Import the login schema
 import { CiUser } from "react-icons/ci";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
@@ -11,83 +11,83 @@ import InputField from "../../component/inputField";
 import Button from "../../component/button";
 import Image from "next/image";
 
-export default function Signup() {
+export default function Login() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: zodResolver(signupSchema),
+    resolver: zodResolver(loginSchema),
   });
 
-  // This will now log the form data correctly
   const onSubmit = (data) => {
-    console.log("Form Data:", data);
+    console.log("Form Data:", data); // successful form submission
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <div className="w-3/4 flex items-center justify-between p-4">
+        <div className="text-blue-500  text-4xl font-bold">LOGO</div>
+        <div>
+          <Button className="w-full">Signup</Button>
+        </div>
+      </div>
+
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-md">
-        <h2 className="text-2xl font-bold text-center text-blue-600">Signup</h2>
+        <h1 className="text-4xl font-bold text-center text-blue-600">Login</h1>
+
         <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-          <InputField
-            id="firstName"
-            label="First Name"
-            type="text"
-            register={register}
-            error={errors.firstName}
-            IconComponent={CiUser}
-          />
-          <InputField
-            id="lastName"
-            label="Last Name"
-            type="text"
-            register={register}
-            error={errors.lastName}
-            IconComponent={CiUser}
-          />
           <InputField
             id="email"
             label="Email"
+            placeholder="Enter your email address"
             type="email"
+            IconComponent={MdEmail}
             register={register}
             error={errors.email}
-            IconComponent={MdEmail}
           />
           <InputField
             id="password"
             label="Create Password"
             type="password"
+            placeholder="Enter your password"
+            IconComponent={RiLockPasswordLine}
             register={register}
             error={errors.password}
-            IconComponent={RiLockPasswordLine}
           />
-          <InputField
-            id="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            register={register}
-            error={errors.confirmPassword}
-            IconComponent={RiLockPasswordLine}
-          />
-          <Button type="submit">Signup</Button>
+          <div className="flex justify-between text-center">
+            <a
+              href="#"
+              className="hover:underline text-xl text-left text-gray-600"
+            >
+              <input type="radio" />
+              <span> </span>Remember me
+            </a>
+            <p>
+              <a
+                href="#"
+                className="text-right text-xl text-blue-600 font-bold hover:underline"
+              >
+                Forget Password
+              </a>
+            </p>
+          </div>
+          <Button type="submit">Login</Button>
         </form>
 
         <div className="text-center">
           <p className="text-sm text-gray-600">
-            Already have an account?{" "}
-            <a href="#" className="text-blue-600 hover:underline">
-              Login
+            Don't have an account?{" "}
+            <a href="#" className="text-blue-600 font-bold hover:underline">
+              Signup
             </a>
           </p>
         </div>
-
         <div className="flex items-center justify-center space-x-2">
           <hr className="w-1/3 border-gray-300" />
           <span className="text-gray-500">Or</span>
           <hr className="w-1/3 border-gray-300" />
         </div>
-
         <button
           type="button"
           className="w-full px-4 py-2 font-thin text-gray-700 bg-white border rounded-md shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
@@ -97,9 +97,9 @@ export default function Signup() {
             alt="Google"
             width={500}
             height={200}
-            className="inline w-5 h-5 mr-2"
+            className=" font-xl inline w-5 h-5 mr-2"
           />
-          Sign in with Google
+          Login with Google
         </button>
       </div>
     </div>
--- my-app/src/app/(pages)/signup/page.tsx
+++ my-app/src/app/(pages)/signup/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { loginSchema } from "../../schemas/loginSchema"; // Import the login schema
+import { signupSchema } from "../../schemas/signupSchema";
 import { CiUser } from "react-icons/ci";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
@@ -11,81 +10,83 @@ import InputField from "../../component/inputField";
 import Button from "../../component/button";
 import Image from "next/image";
 
-export default function Login() {
+export default function Signup() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: zodResolver(loginSchema),
+    resolver: zodResolver(signupSchema),
   });
 
+  // This will now log the form data correctly
   const onSubmit = (data) => {
-    console.log("Form Data:", data); // successful form submission
+    console.log("Form Data:", data);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <div className="w-3/4 flex items-center justify-between p-4">
-        <div className="text-blue-500 text-2xl font-bold">LOGO</div>
-        <div>
-          <Button className="w-full text-blue-500">Signup</Button>
-        </div>
-      </div>
-
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-md">
-        <h1 className="text-2xl font-bold text-center text-blue-600">Login</h1>
-
+        <h2 className="text-4xl font-bold text-center text-blue-600">Signup</h2>
         <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <InputField
+            id="firstName"
+            label="First Name"
+            type="text"
+            register={register}
+            error={errors.firstName}
+            IconComponent={CiUser}
+          />
+          <InputField
+            id="lastName"
+            label="Last Name"
+            type="text"
+            register={register}
+            error={errors.lastName}
+            IconComponent={CiUser}
+          />
           <InputField
             id="email"
             label="Email"
             type="email"
-            IconComponent={MdEmail}
             register={register}
             error={errors.email}
+            IconComponent={MdEmail}
           />
           <InputField
             id="password"
             label="Create Password"
             type="password"
-            IconComponent={RiLockPasswordLine}
             register={register}
             error={errors.password}
+            IconComponent={RiLockPasswordLine}
           />
-          <div className="flex justify-between text-center">
-            <a
-              href="#"
-              className="hover:underline text-sm text-left text-gray-600"
-            >
-              <input type="radio" />
-              <span> </span>Remember me
-            </a>
-            <p>
-              <a
-                href="#"
-                className="text-right text-blue-600 font-bold hover:underline"
-              >
-                Forget Password
-              </a>
-            </p>
-          </div>
-          <Button type="submit">Login</Button>
+          <InputField
+            id="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            register={register}
+            error={errors.confirmPassword}
+            IconComponent={RiLockPasswordLine}
+          />
+          <Button type="submit">Signup</Button>
         </form>
 
         <div className="text-center">
-          <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
-            <a href="#" className="text-blue-600 font-bold hover:underline">
-              Signup
+          <p className="text-xl text-gray-600">
+            Already have an account?{" "}
+            <a href="#" className="text-blue-600 text-xl hover:underline">
+              Login
             </a>
           </p>
         </div>
+
         <div className="flex items-center justify-center space-x-2">
           <hr className="w-1/3 border-gray-300" />
           <span className="text-gray-500">Or</span>
           <hr className="w-1/3 border-gray-300" />
         </div>
+
         <button
           type="button"
           className="w-full px-4 py-2 font-thin text-gray-700 bg-white border rounded-md shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
@@ -95,9 +96,9 @@ export default function Login() {
             alt="Google"
             width={500}
             height={200}
-            className="inline w-5 h-5 mr-2"
+            className="text-xl inline w-5 h-5 mr-2"
           />
-          Login with Google
+          Sign in with Google
         </button>
       </div>
     </div>
